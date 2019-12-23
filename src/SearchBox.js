aa5508ab5113ba8fffc58de02b2534f71af78b2c
unknown <amartyaroykumar001@gmail.com>
2019-12-23 09:59:56 +0530

    first react app ROBOFRIENDS

---
+++ src/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBox=()=>{
+    return(
+        <div>
+            <input className="pa3 ba  b--green bg-lightest-blue" type="search" placeholder="enter your robot name"></input>
+        </div>
+
+    );
+
+}
+export default SearchBox;
